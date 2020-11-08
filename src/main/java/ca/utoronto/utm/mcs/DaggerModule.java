22f0cc7611e4c60d390866233d697a0eb27ca44a
ParekhDevanshi <43529601+ParekhDevanshi@users.noreply.github.com>
2020-11-08 01:58:37 -0500

    got dependency injection working

--- src/main/java/ca/utoronto/utm/mcs/DaggerModule.java
+++ src/main/java/ca/utoronto/utm/mcs/DaggerModule.java
@@ -12,17 +12,47 @@ import com.mongodb.MongoClientSettings;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 @Module
 public class DaggerModule {
+
+    // private static 
+    static int port = 8080;
 	
     @Provides public MongoClient provideMongoClient() {
         /* TODO: Fill in this function */
-    	return null;
+
+        MongoClient mongoClient = MongoClients.create();
+
+        // MongoClient mongoClient = new MongoClient();
+        // MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+
+        // MongoDatabase database = mongoClient.getDatabase("csc301a2");
+        // MongoCollection<Document> collection = database.getCollection("posts");
+
+        // confused about what to return... collection or the client or collection 
+        // i'm gonna go with database since there is a get and set DB in Dagger.java
+        // nvm got to return mongoClient
+
+    	return mongoClient;
     }
 
-    @Provides public HttpServer provideHttpServer() {
+    // added the throws IOException, not sure if i should do a try catch...
+    // confrimed from piazza and trial and error can't throw it need to try catch
+    @Provides public HttpServer provideHttpServer(){
         /* TODO: Fill in this function */
-        return null;
+
+        HttpServer server = null;
+
+        try{
+            server = HttpServer.create(new InetSocketAddress("0.0.0.0", port), 0);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }        
+
+        return server;
     }
 }
