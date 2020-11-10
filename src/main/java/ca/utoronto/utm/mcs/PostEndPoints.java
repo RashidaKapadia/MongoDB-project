e456de9bca0d403d92e6d53c201317add7af1889
ParekhDevanshi <43529601+ParekhDevanshi@users.noreply.github.com>
2020-11-10 01:15:13 -0500

    removed commets and print statements

--- src/main/java/ca/utoronto/utm/mcs/PostEndPoints.java
+++ src/main/java/ca/utoronto/utm/mcs/PostEndPoints.java
@@ -46,8 +46,6 @@ public class PostEndPoints implements HttpHandler {
         MongoDatabase database = db.getDatabase("csc301a2");
         MongoCollection<Document> collection = database.getCollection("posts");
 
-        System.out.println(r.getRequestMethod());
-
         if (r.getRequestMethod().equals("PUT")) {
             try {
                 putPost(r, collection);
@@ -56,7 +54,6 @@ public class PostEndPoints implements HttpHandler {
             }
         } else if (r.getRequestMethod().equals("GET")) {
             try {
-                System.out.println("before going to getPost");
                 getPost(r, collection);
             } catch (Exception e) {
                 e.printStackTrace();
@@ -72,82 +69,13 @@ public class PostEndPoints implements HttpHandler {
         }
     }
 
-    public void putPost(HttpExchange r, MongoCollection<Document> collection) throws IOException, JSONException {
-        // Convert Body to JSON Object
-        String title = "";
-        String author = "";
-        String content = "";
-        ArrayList<String> tags;
-        JSONObject deserialized = new JSONObject();
+    public void putPost(HttpExchange r, MongoCollection<Document> collection) {
         try {
-            String body = Utils.convert(r.getRequestBody());
-            deserialized = new JSONObject(body);
-        } catch (JSONException e) {
-            r.sendResponseHeaders(400, -1);
-            return;
-        } catch (IOException e) {
-            r.sendResponseHeaders(500, -1);
-            return;
-        }
-
-        // Get Data from JSON
-        // title
-        if (deserialized.has("title")) {
-            title = deserialized.getString("title");
-        } else {
-            r.sendResponseHeaders(400, -1);
-            return;
-        }
-        // author
-        if (deserialized.has("author")) {
-            author = deserialized.getString("author");
-        } else {
-            r.sendResponseHeaders(400, -1);
-            return;
-        }
-        // content
-        if (deserialized.has("content")) {
-            content = deserialized.getString("content");
-            System.out.println(content);
-        } else {
-            r.sendResponseHeaders(400, -1);
-            return;
-        }
-        // tags
-        if (deserialized.has("tags")) {
-            JSONArray getTags = deserialized.getJSONArray("tags");
-            tags = jsonArrayToArrayList(getTags);
-            System.out.println(tags);
-            System.out.println(author);
-            System.out.println(content);
-        } else {
-            r.sendResponseHeaders(400, -1);
-            return;
-        }
-
-        // Create Mongodb Document
-        Document post = new Document("title", title).append("author", author).append("content", content).append("tags",
-                tags);
-
-        collection.insertOne(post);
-
-        ObjectId id = (ObjectId) post.get("_id");
-        String response = "{\"_id\": \"" + id.toString() + "\"}";
-
-        r.sendResponseHeaders(200, response.length());
-        OutputStream os = r.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
-    }
-
-    public void deletePost(HttpExchange r, MongoCollection<Document> collection) {
-        try {
-            System.out.println("reached here");
             // Convert Body to JSON Object
             String title = "";
-            String _id = "";
-            String response = "";
-
+            String author = "";
+            String content = "";
+            ArrayList<String> tags;
             JSONObject deserialized = new JSONObject();
             try {
                 String body = Utils.convert(r.getRequestBody());
@@ -161,18 +89,48 @@ public class PostEndPoints implements HttpHandler {
             }
 
             // Get Data from JSON
-            // id
-            if (deserialized.has("_id")) {
-                _id = deserialized.getString("_id");
+            // title
+            if (deserialized.has("title")) {
+                title = deserialized.getString("title");
+            } else {
+                r.sendResponseHeaders(400, -1);
+                return;
+            }
+            // author
+            if (deserialized.has("author")) {
+                author = deserialized.getString("author");
+            } else {
+                r.sendResponseHeaders(400, -1);
+                return;
+            }
+            // content
+            if (deserialized.has("content")) {
+                content = deserialized.getString("content");
+            } else {
+                r.sendResponseHeaders(400, -1);
+                return;
+            }
+            // tags
+            if (deserialized.has("tags")) {
+                JSONArray getTags = deserialized.getJSONArray("tags");
+                tags = jsonArrayToArrayList(getTags);
             } else {
                 r.sendResponseHeaders(400, -1);
                 return;
             }
 
-            DeleteResult myDoc = collection.deleteOne(eq("_id", new ObjectId(_id)));
-            r.sendResponseHeaders(200, 0);
+            // Create Mongodb Document
+            Document post = new Document("title", title).append("author", author).append("content", content)
+                    .append("tags", tags);
+
+            collection.insertOne(post);
+
+            ObjectId id = (ObjectId) post.get("_id");
+            String response = "{\"_id\": \"" + id.toString() + "\"}";
+
+            r.sendResponseHeaders(200, response.length());
             OutputStream os = r.getResponseBody();
-            os.write("".getBytes());
+            os.write(response.getBytes());
             os.close();
 
         } catch (IOException e) {
@@ -184,11 +142,11 @@ public class PostEndPoints implements HttpHandler {
 
     public void getPost(HttpExchange r, MongoCollection<Document> collection) {
         try {
-            System.out.println("reached here");
-            // Convert Body to JSON Object
             String _id = "";
+            String title = "";
             String response = "";
 
+            // Convert Body to JSON Object
             JSONObject deserialized = new JSONObject();
             try {
                 String body = Utils.convert(r.getRequestBody());
@@ -214,18 +172,11 @@ public class PostEndPoints implements HttpHandler {
                 return;
             }
 
-            // if (title.equals("") && _id.equals("")){
-            // return;
-            // }
-
             if (_id == "") {
                 MongoCursor<Document> cursor = collection.find(eq("title", title)).iterator();
                 try {
                     response = "[";
                     while (cursor.hasNext()) {
-                        // System.out.println(cursor.next().toJson());
-                        System.out.println(response);
-
                         response = response + cursor.next().toJson() + " , ";
                     }
                 } finally {
@@ -233,20 +184,62 @@ public class PostEndPoints implements HttpHandler {
                 }
                 response = response.substring(0, response.length() - 3);
                 response = response + "]";
-                System.out.println("______________________________________________");
-                System.out.println(response);
             } else {
                 System.out.println(_id);
                 Document myDoc = collection.find(eq("_id", new ObjectId(_id))).first();
-                System.out.println(myDoc.toJson());
                 response = myDoc.toJson();
-                System.out.println(response);
             }
 
             byte[] bs = response.getBytes("UTF-8");
             r.sendResponseHeaders(200, bs.length);
             OutputStream os = r.getResponseBody();
             os.write(bs);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deletePost(HttpExchange r, MongoCollection<Document> collection) {
+        try {
+
+            String title = "";
+            String _id = "";
+            String response = "";
+
+            // Convert Body to JSON Object
+            JSONObject deserialized = new JSONObject();
+            try {
+                String body = Utils.convert(r.getRequestBody());
+                deserialized = new JSONObject(body);
+            } catch (JSONException e) {
+                r.sendResponseHeaders(400, -1);
+                return;
+            } catch (IOException e) {
+                r.sendResponseHeaders(500, -1);
+                return;
+            }
+
+            // Get Data from JSON
+            // id
+            if (deserialized.has("_id")) {
+                _id = deserialized.getString("_id");
+            } else {
+                r.sendResponseHeaders(400, -1);
+                return;
+            }
+
+            DeleteResult myDoc = collection.deleteOne(eq("_id", new ObjectId(_id)));
+            if (myDoc.getDeletedCount() == 0) {
+                r.sendResponseHeaders(404, -1);
+                return;
+            }
+            r.sendResponseHeaders(200, 0);
+            OutputStream os = r.getResponseBody();
+            os.write("".getBytes());
+            os.close();
 
         } catch (IOException e) {
             e.printStackTrace();
